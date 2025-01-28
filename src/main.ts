e84e7e8f41ddb60e4c0f28922bcd96de07394f42
JayBains <jayarjunbains@gmail.com>
2025-01-28 13:43:17 +0000

    Improved variable and class names - removed redundant code - updated config

--- src/main.ts
+++ src/main.ts
@@ -29,27 +29,30 @@ if (
 }
 
 window.addEventListener("resize", (event) => {
-  prect = paddle.getBoundingClientRect();
   rect = game.getBoundingClientRect();
   return event;
+});
+
+button.addEventListener("click", () => {
+  run();
 });
 
 const run = () => {
   home.style.display = "none";
   container.style.display = "flex";
 
-  const audio = new Audio(bounceSound);
   prect = paddle.getBoundingClientRect();
   rect = game.getBoundingClientRect();
 
+  const audio = new Audio(bounceSound);
   const newX = 150;
   const newY = 20;
   const StartSpd = 3;
   const acceleration = 1.02;
-  let x = newX;
-  let y = newY;
   let speedX = Math.round(Math.random()) ? -1 * StartSpd : StartSpd;
   let speedY = Math.round(Math.random()) ? -1 * StartSpd : StartSpd;
+  let ballX = newX;
+  let ballY = newY;
   let paddleX = 0;
   let score = 0;
   let highscore = 0;
@@ -71,20 +74,23 @@ const run = () => {
     }
   });
 
-  function animate() {
+  function physics() {
     if (!ball || !paddle || !scoreCounter) {
-      throw new Error("animate() error: Cannot find ball, paddle or score");
+      throw new Error("physics() error: Cannot find ball, paddle or score");
     }
-    x += speedX;
-    y += speedY;
-    if (x - 5 <= 0 && speedX < 0) {
+    ballX += speedX;
+    ballY += speedY;
+    if (ballX - 5 <= 0 && speedX < 0) {
       speedX = -speedX;
-    } else if (x + 25 >= rect.right - rect.left && speedX > 0) {
+    } else if (ballX + 25 >= rect.right - rect.left && speedX > 0) {
       speedX = -speedX;
-    } else if (y - 5 <= 0 && speedY < 0) {
+    } else if (ballY - 5 <= 0 && speedY < 0) {
       speedY = -speedY;
-    } else if (y + 25 >= rect.bottom - rect.top && speedY > 0) {
-      if (x + 15 > paddleX && x + 5 < paddleX + prect.right - prect.left) {
+    } else if (ballY + 25 >= rect.bottom - rect.top && speedY > 0) {
+      if (
+        ballX + 15 > paddleX &&
+        ballX + 5 < paddleX + prect.right - prect.left
+      ) {
         audio.play();
         speedY = -speedY;
         score++;
@@ -92,50 +98,41 @@ const run = () => {
         speedX *= acceleration;
         speedY *= acceleration;
       } else {
-        respawn();
+        gameOver();
       }
     }
-    ball.style.left = x + "px";
-    ball.style.top = y + "px";
+    ball.style.left = ballX + "px";
+    ball.style.top = ballY + "px";
   }
 
-  function sleep(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-
-  function respawn() {
+  function gameOver() {
     if (!highscoreCounter || !scoreCounter || !ball) {
-      throw new Error("respawn() error: cannot find scores or ball");
+      throw new Error("gameOver() error: cannot find scores or ball");
     }
     speedX = 0;
     speedY = 0;
     ball.style.backgroundColor = "red";
-    sleep(200).then(() => {
+    setTimeout(() => {
       if (score > highscore) {
         highscore = score;
         highscoreCounter.textContent = `Highscore: ${highscore}`;
         alert(`New high score! You got: ${highscore}!`);
       }
-
-      sleep(1000).then(() => {
+      setTimeout(() => {
         ball.style.backgroundColor = "white";
         score = 0;
         scoreCounter.textContent = `Score: ${score}`;
-        x = newX;
-        y = newY;
+        ballX = newX;
+        ballY = newY;
         speedX = Math.round(Math.random()) ? -1 * StartSpd : StartSpd;
         speedY = Math.round(Math.random()) ? -1 * StartSpd : StartSpd;
-      });
-    });
+      }, 1000);
+    }, 200);
   }
 
   function gameLoop() {
-    animate();
+    physics();
     requestAnimationFrame(gameLoop);
   }
   requestAnimationFrame(gameLoop);
 };
-
-button.addEventListener("click", () => {
-  run();
-});
